88af88dac38daee4b1b5778b740f5f0f6443b900
Carlos <duducp2013@gmail.com>
2020-03-14 19:17:20 -0300

    Ajustes

--- optimize.js
+++ optimize.js
@@ -1,3 +1,4 @@
+// jshint ignore: start
 'use strict';
 
 const AWS = require('aws-sdk');
@@ -8,25 +9,28 @@ const S3 = new AWS.S3();
 
 module.exports.handle = async ({ Records: records }, context) => {
   try {
-    await Promisse.all(records.map(async record => {
-      const { key } = record.s3.object;
-      const image = await s3.getObject({
-        Bucket: ProcessingInstruction.env.bucket,
-        Key: key,
-      }).promise();
+    /** Promisse.all aguarda todas as promisses finalizarem */
+    await Promisse.all(
+      records.map(async record => {
+        const { key } = record.s3.object;
+        const image = await s3.getObject({
+          Bucket: ProcessingInstruction.env.bucket,
+          Key: key,
+        }).promise();
 
-      const optimezed = await sharp(image.Body)
-        .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
-        .toFormat('jpeg', { progressive: true, quality: 50 })
-        .toBuffer()
+        const optimezed = await sharp(image.Body)
+          .resize(1280, 720, { fit: 'inside', withoutEnlargement: true })
+          .toFormat('jpeg', { progressive: true, quality: 50 })
+          .toBuffer()
 
-      await S3.putObject({
-        Body: optimezed,
-        Bucket: process.env.bucket,
-        ContentType: 'image/jpeg',
-        Key: `compressed/${basename(key, extname(key))}.jpg`
+        await S3.putObject({
+          Body: optimezed,
+          Bucket: process.env.bucket,
+          ContentType: 'image/jpeg',
+          Key: `compressed/${basename(key, extname(key))}.jpg`
+        })
       })
-    }));
+    );
 
     return {
       statusCode: 301,
